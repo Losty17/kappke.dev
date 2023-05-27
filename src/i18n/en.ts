e8b187e8db548273ac80b172b13ac55a98da7736
Losty <vinikappke@gmail.com>
2023-05-26 23:05:53 -0300

    i18n

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -2,8 +2,24 @@ import { Globalizer } from ".";
 
 export default {
   greeting: {
-    welcome: "Welcome",
-    to: "to",
+    welcome: "Bem-vindo",
+    to: "a",
   },
-  seeMore: "See more",
+  seeMore: "Veja mais",
+  whoAmI: {
+    title: "Quem sou eu?",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere expedita placeat quis quod odio iusto, quae, asperiores totam harum reiciendis nisi iste voluptatem earum ipsa cum quibusdam, quas mollitia in?",
+  },
+  myProjects: {
+    title: "Meus projetos",
+    content: "",
+  },
+  recentPosts: {
+    title: "Posts recentes",
+    empty: "Nenhum post encontrado",
+  },
+  contactMe: {
+    title: "Contato",
+  },
 } as Globalizer;
